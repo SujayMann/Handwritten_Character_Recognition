e7b22da6906c25fe74a3235e32c09a25d7949d05
Sujay Mann <92069576+SujayMann@users.noreply.github.com>
2024-12-28 21:48:31 +0530

    Add files via upload

--- image_classifier.py
+++ image_classifier.py
@@ -6,6 +6,8 @@ from tensorflow import keras
 from keras.models import load_model
 import string
 
+st.title('Image Classifier')
+
 model = load_model('model.keras')
 digits = [str(i) for i in range(10)]
 uppercase = [i for i in string.ascii_uppercase]
@@ -13,9 +15,10 @@ lowercase = [i for i in string.ascii_lowercase]
 class_names = digits + uppercase + lowercase
 
 def predict_image(img_file):
-    image = tf.keras.utils.load_img(img_file, target_size=(256, 256))
+    image = tf.keras.utils.load_img(img_file, target_size=(224, 224))
     input_arr = keras.utils.img_to_array(image)
     input_arr = tf.expand_dims(input_arr, axis=0)
+    input_arr = input_arr / 255.0
     pred = model.predict(input_arr)
     class_index = np.argmax(pred, axis=1)[0]
     result = class_names[class_index]
